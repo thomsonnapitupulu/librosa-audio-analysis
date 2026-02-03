82c3b6505cb680874c7cd98758c609339e9a67f9
thomson.napitupulu <thomson.p.napitupulu@gmail.com>
2026-02-03 15:26:20 +0700

    Add MFCC extraction and inspect feature shape

--- src/tutorial/mfcc.py
+++ src/tutorial/mfcc.py
@@ -5,3 +5,12 @@ y, sr = librosa.load(librosa.example("nutcracker"))
 
 mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
 print("MFCC shape:", mfccs.shape)
+
+duration = mfccs.shape[1] * 512 / sr
+print("MFCC duration: ", duration)
+
+mfcc_mean = mfccs.mean(axis=1)
+mfcc_std = mfccs.std(axis=1)
+
+print("MFCC mean:", mfcc_mean)
+print("MFCC Std:", mfcc_std)
