5222d484348dd0b0f70ce4323ff6a1e744e7f8ea
thomson.napitupulu <thomson.p.napitupulu@gmail.com>
2026-02-03 12:50:37 +0700

    Add beat tracking example

--- src/tutorial/quickstart.py
+++ src/tutorial/quickstart.py
@@ -5,3 +5,6 @@ y, sr = librosa.load(filename)
 
 print(f"Audio shape: {y.shape}")
 print(f"Sample rate: {sr}")
+
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+print(f"Estimated tempo: {tempo:.2f} BPM")
