d9207e19565721edbc6e73572b0244cf2cbe6e52
thomson.napitupulu <thomson.p.napitupulu@gmail.com>
2026-02-03 15:16:34 +0700

    Fix float type value issue

--- src/tutorial/quickstart.py
+++ src/tutorial/quickstart.py
@@ -7,4 +7,6 @@ print(f"Audio shape: {y.shape}")
 print(f"Sample rate: {sr}")
 
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-print(f"Estimated tempo: {tempo:.2f} BPM")
+tempo_value = float(tempo)
+print(f"Estimated tempo: {tempo_value:.2f} BPM")
+
